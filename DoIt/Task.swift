bd00ded0437cb1997f9849d0dde6c9417326646b
Michael Baumgaertner <Michael@Michaels-Air-2.localdomain>
2017-12-28 13:38:25 +0100

    Working1

---
+++ DoIt/Task.swift
@@ -0,0 +1,13 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Michael Baumgaertner on 28.12.17.
+//  Copyright © 2017 Michael Baumgaertner. All rights reserved.
+//
+
+import Foundation
+class Task {
+    var name = ""
+    var important = false
+}
